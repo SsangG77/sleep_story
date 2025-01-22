ec0185cbb8e9995a34a0d529ebc5fdda7a49b51a
sangjin <sangjincha719@gmail.com>
2025-01-22 13:48:35 +0900

    Initial Commit

---
+++ SleepStory/SleepStoryApp.swift
@@ -0,0 +1,32 @@
+//
+//  SleepStoryApp.swift
+//  SleepStory
+//
+//  Created by 차상진 on 1/22/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SleepStoryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SleepStoryTests/SleepStoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  SleepStoryTests.swift
+//  SleepStoryTests
+//
+//  Created by 차상진 on 1/22/25.
+//
+
+import Testing
+@testable import SleepStory
+
+struct SleepStoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
