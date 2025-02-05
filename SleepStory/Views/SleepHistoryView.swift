364b22bfd41cbd33b110840612fe075bff96576a
sangjin <63487642+SsangG77@users.noreply.github.com>
2025-02-05 18:48:46 +0900

    Update SleepHistoryView.swift

--- SleepStory/Views/SleepHistoryView.swift
+++ SleepStory/Views/SleepHistoryView.swift
@@ -7,12 +7,40 @@
 
 import SwiftUI
 
+
 struct SleepHistoryView: View {
+    @ObservedObject var sleepManager: SleepManager
+    @State private var showingDeleteAlert = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(sleepManager.sleepHistory) { sleepData in
+                    SleepHistoryCard(sleepData: sleepData)
+                }
+            }
+            .navigationTitle("수면 기록")
+            .listStyle(InsetGroupedListStyle())
+            .toolbar {
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                Button(action: {
+                                    showingDeleteAlert = true
+                                }) {
+                                    Image(systemName: "trash")
+                                        .foregroundColor(.red)
+                                }
+                            }
+                        }
+                        .alert("수면 기록 삭제", isPresented: $showingDeleteAlert) {
+                            Button("취소", role: .cancel) { }
+                            Button("삭제", role: .destructive) {
+                                sleepManager.clearAllSleepHistory()
+                            }
+                        } message: {
+                            Text("모든 수면 기록이 삭제됩니다.\n삭제된 기록은 복구할 수 없습니다.")
+                        }
+        }
     }
 }
 
-#Preview {
-    SleepHistoryView()
-}
+
