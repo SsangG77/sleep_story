0377f7d2bfd72bd4bccb36c84e020d5658176cf9
sangjin <63487642+SsangG77@users.noreply.github.com>
2025-02-05 18:47:45 +0900

    Update NoDataView.swift

--- SleepStory/Views/NoDataView.swift
+++ SleepStory/Views/NoDataView.swift
@@ -7,9 +7,19 @@
 
 import SwiftUI
 
+
+// 데이터 없을 때 표시할 뷰
 struct NoDataView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            Image(systemName: "bed.double.fill")
+                .font(.system(size: 40))
+                .foregroundColor(.gray)
+            Text("아직 수면 데이터가 없어요")
+                .font(.headline)
+                .foregroundColor(.secondary)
+        }
+        .padding()
     }
 }
 
