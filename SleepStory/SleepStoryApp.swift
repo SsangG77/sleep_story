3b33953b445c5c41d13463a54ca8d1be5ee6badf
sangjin <63487642+SsangG77@users.noreply.github.com>
2025-02-05 18:50:49 +0900

    Update SleepStoryApp.swift

--- SleepStory/SleepStoryApp.swift
+++ SleepStory/SleepStoryApp.swift
@@ -10,23 +10,21 @@ import SwiftData
 
 @main
 struct SleepStoryApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+    
+    let sleepManager = SleepManager()
 
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
+    init() {
+        sleepManager.requestAuthorization()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
+        
     }
+}
+
+#Preview {
+    ContentView()
 }
