01afccc7d935653b08ef0825288f273e222532e7
sangjin <63487642+SsangG77@users.noreply.github.com>
2025-02-05 18:47:20 +0900

    Update HeaderView.swift

--- SleepStory/Views/HeaderView.swift
+++ SleepStory/Views/HeaderView.swift
@@ -7,12 +7,18 @@
 
 import SwiftUI
 
+// 헤더 뷰
 struct HeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "moon.stars.fill")
+                .font(.system(size: 50))
+                .foregroundColor(.indigo)
+                .padding(.bottom, 10)
+            Text("당신의 수면을 이야기로 들려드려요")
+                .font(.headline)
+                .foregroundColor(.secondary)
+        }
+        .padding()
     }
-}
-
-#Preview {
-    HeaderView()
 }
