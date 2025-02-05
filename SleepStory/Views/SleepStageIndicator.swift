1ff69f235208e701963ace09b081fdc134fba3c7
sangjin <63487642+SsangG77@users.noreply.github.com>
2025-02-05 18:49:16 +0900

    Update SleepStageIndicator.swift

--- SleepStory/Views/SleepStageIndicator.swift
+++ SleepStory/Views/SleepStageIndicator.swift
@@ -7,12 +7,28 @@
 
 import SwiftUI
 
+
+// 수면 단계 표시 뷰
 struct SleepStageIndicator: View {
+    let stage: SleepStage
+    //rem ,deep, light, awake
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 10)
+            .frame(width: 15, height: stage == .rem ? 50 : stage == .deep ? 80 : stage == .light ? 100 : 120)
+            .foregroundColor(backgroundColor)
+    }
+    
+    private var backgroundColor: Color {
+        switch stage {
+        case .awake: return .gray
+        case .light: return .blue
+        case .deep: return .indigo
+        case .rem: return .purple
+        }
     }
 }
 
 #Preview {
-    SleepStageIndicator()
+    SleepStageIndicator(stage: .rem)
 }
